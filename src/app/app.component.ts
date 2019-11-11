a6317c49726274b1fcb9f45e18a65f0d7068b5f3
atheodosiou <anastasios.theodosiou@gmail.com>
2019-11-11 22:39:08 +0200

    Adds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: "app-root",
@@ -6,5 +8,13 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
+  constructor(private router:Router, private authService:AuthService){
+    // this.router.events.subscribe(event=>{
+    //   if(event instanceof NavigationEnd) {
+    //     console.log('ROUTER EVENT:', event)
+    //     console.log(this.authService.isLogedIn)
+    //   }     
+    // })
+  }
   title = "parse-dashboard";
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Input, OnChanges } from "@angular/core";
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -7,15 +7,22 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ["./navbar.component.scss"],
   // changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent implements OnInit {
-  logedIn=false;
+export class NavbarComponent implements OnInit{
+  
+  // TO-DO
+  // I have to update the logedIn stare in order to chande the navbar links!
+  @Input() logedIn=false;
+
   constructor(private authService:AuthService, 
     // private cdr:ChangeDetectorRef
     ) {
-    this.logedIn = this.authService.isLogedIn;
+      // this.logedIn = this.authService.isLogedIn;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.logedIn = this.authService.isLogedIn;
+    console.log(this.logedIn)
+  }
 
   logOut(){
     this.authService.logOut();
