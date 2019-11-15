1f82ad76da6323c838fe4deaccf9f5f98d934c19
atheodosiou <anastasios.theodosiou@gmail.com>
2019-11-15 21:11:17 +0200

    update environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ import { javaScriptKey } from 'parse';
 export const environment = {
   production: true,
   // serverURL:'http://localhost:4242/parse',
-  serverURL: 'http://167.71.56.69/parse',
+  serverURL: 'http://interviewfront.theodosiou-anastasios.com:4242/parse',
   appId: 'instashopInterviewAppId',
   googleMapsApiKey: 'AIzaSyBmi_OGF0-_U5oZ77rug2wbVsN8cA_7ge4',
-  javaScriptKey:'instashopJavascriptKey'
+  javaScriptKey: 'instashopJavascriptKey'
 };
