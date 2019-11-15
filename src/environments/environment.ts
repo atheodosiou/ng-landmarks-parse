ffc491974a5d478dc00ddf971740467d725af460
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-11-15 15:22:01 +0200

    Build for deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   // serverURL: 'http://localhost:4242/parse',
-  serverURL: 'http://167.71.56.69/parse',
+  serverURL: 'http://167.71.56.69:4242/parse/',
   appId: 'instashopInterviewAppId',
   googleMapsApiKey: 'AIzaSyBmi_OGF0-_U5oZ77rug2wbVsN8cA_7ge4',
   javaScriptKey: 'instashopJavascriptKey'
