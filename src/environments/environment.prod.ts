513f3c746ea1bca1197c225bb17eccbc2b2aa151
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-11-15 15:22:34 +0200

    Fix environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { javaScriptKey } from 'parse';
 export const environment = {
   production: true,
   // serverURL:'http://localhost:4242/parse',
-  serverURL: 'http://167.71.56.69:4242/parse/',
+  serverURL: 'http://167.71.56.69:4242/parse',
   appId: 'instashopInterviewAppId',
   googleMapsApiKey: 'AIzaSyBmi_OGF0-_U5oZ77rug2wbVsN8cA_7ge4',
   javaScriptKey:'instashopJavascriptKey'
